9baeaa3c9f49ac3b17646dcb1a0ae1d7a7a44e50
Silvi Sinanaj <silviberat@gmail.com>
2021-04-15 19:56:17 +0200

    done

--- index.js
+++ index.js
@@ -21,7 +21,32 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    let recipe = data[0]
+    return Recipe.create(recipe)
+  })
+  .then((addedRecipe) => {
+    console.log("Data Added: " + addedRecipe.title)
+    return Recipe.insertMany(data)
+  })
+  .then((addedTitles) => {
+    addedTitles.forEach(elem => {
+      console.log(elem.title)
+    })
+
+    return Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  })
+  .then(() => {
+    console.log('Duration Updated Succesfully')
+    return Recipe.deleteOne({ title: "Carrot Cake" })
+  })
+  .then(() => {
+    console.log("Carrot Cake finished")
+    return mongoose.connection.close()
+  })
+  .then(() => {
+    console.log("Terminated")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
